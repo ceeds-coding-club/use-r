e724f65ff05fe10e18d2a5a0e5ea76ab0f136c62
James Robinson <jpwrobinson@gmail.com>
2022-06-27 09:06:16 +0200

    wrong commit

--- tidytuesday_Jun22.R
+++
@@ -1,3 +0,0 @@
-
-firsts <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-09/firsts.csv')
-science <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-09/science.csv')
